398282e7e128b60a652f2e9519e8c038d7846e00
chatflip <oo.chat.flip@gmail.com>
2021-04-07 02:32:06 +0900

    pysen run format

--- classification/py/main.py
+++ classification/py/main.py
@@ -1,14 +1,14 @@
 import os
 
+import pytorch_lightning as pl
 import torch
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
 from torchvision import transforms
-import pytorch_lightning as pl
+from torchvision.datasets import MNIST
+
 
 class MNISTModel(pl.LightningModule):
-
     def __init__(self):
         super(MNISTModel, self).__init__()
         self.l1 = torch.nn.Linear(28 * 28, 10)
@@ -19,16 +19,25 @@ class MNISTModel(pl.LightningModule):
     def training_step(self, batch, batch_nb):
         x, y = batch
         loss = F.cross_entropy(self(x), y)
-        tensorboard_logs = {'train_loss': loss}
-        return {'loss': loss, 'log': tensorboard_logs}
+        tensorboard_logs = {"train_loss": loss}
+        return {"loss": loss, "log": tensorboard_logs}
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=0.02)
 
 
 if __name__ == "__main__":
-    train_loader = DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()), batch_size=64)
+    train_loader = DataLoader(
+        MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()),
+        batch_size=64,
+    )
 
     mnist_model = MNISTModel()
-    trainer = pl.Trainer(gpus=[0, 1], accelerator='ddp', progress_bar_refresh_rate=20, sync_batchnorm=True, precision=16)    
-    trainer.fit(mnist_model, train_loader)  
+    trainer = pl.Trainer(
+        gpus=[0, 1],
+        accelerator="ddp",
+        progress_bar_refresh_rate=20,
+        sync_batchnorm=True,
+        precision=16,
+    )
+    trainer.fit(mnist_model, train_loader)
