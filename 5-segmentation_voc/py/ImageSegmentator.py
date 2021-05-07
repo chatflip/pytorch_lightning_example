de1231784a643304349e45fa15c413d049f6ece0
chatflip <oo.chat.flip@gmail.com>
2021-05-08 01:27:54 +0900

    valのコードをきれいにした

--- 5-segmentation_voc/py/ImageSegmentator.py
+++ 5-segmentation_voc/py/ImageSegmentator.py
@@ -38,53 +38,23 @@ class ImageSegmentator(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         image, target = batch
         output = self(image)
-        jaccard_loss = self.criterions["jaccard_loss"](output, target)
-        dice_loss = self.criterions["dice_loss"](output, target)
-        lovasz_loss = self.criterions["lovasz_loss"](output, target)
-        bce_loss = self.criterions["bce_loss"](output, target)
-        loss = (
-            0.25 * jaccard_loss
-            + 0.25 * dice_loss
-            + 0.25 * lovasz_loss
-            + 0.25 * bce_loss
-        )
+        result_dict = {}
+        loss = 0
+        for key in self.criterions.keys():
+            single_loss = self.criterions[key](output, target)
+            loss += self.criterions_weight[key] * single_loss
+            result_dict[key] = loss.item()
         iou_acc = self.metrics(output, target)
-        return (
-            jaccard_loss.item(),
-            dice_loss.item(),
-            lovasz_loss.item(),
-            bce_loss.item(),
-            loss.item(),
-            iou_acc.item(),
-        )
+        result_dict["loss"] = loss.item()
+        result_dict["iou"] = iou_acc.item()
+        return result_dict
 
     def validation_epoch_end(self, outputs):
-        # TODO: drop_lastの場合計算合わない
-        jaccard_loss_list = []
-        dice_loss_list = []
-        lovasz_loss_list = []
-        bce_loss_list = []
-        loss_list = []
-        iou_acc_list = []
-        for output in outputs:
-            jaccard_loss_list.append(output[0].cpu().numpy())
-            dice_loss_list.append(output[1].cpu().numpy())
-            lovasz_loss_list.append(output[2].cpu().numpy())
-            bce_loss_list.append(output[3].cpu().numpy())
-            loss_list.append(output[4].cpu().numpy())
-            iou_acc_list.append(output[5].cpu().numpy())
-        jaccard_loss = sum(jaccard_loss_list) / len(jaccard_loss_list)
-        dice_loss = sum(dice_loss_list) / len(dice_loss_list)
-        lovasz_loss = sum(lovasz_loss_list) / len(lovasz_loss_list)
-        bce_loss = sum(bce_loss_list) / len(bce_loss_list)
-        loss = sum(loss_list) / len(loss_list)
-        iou_acc = sum(iou_acc_list) / len(iou_acc_list)
-        self.log("val_jaccard_loss", jaccard_loss)
-        self.log("val_dice_loss", dice_loss)
-        self.log("val_lovasz_loss", lovasz_loss)
-        self.log("val_bce_loss", bce_loss)
-        self.log("val_loss", loss)
-        self.log("val_iou", iou_acc)
+        # TODO: drop_last分のbatchsizeの考慮されてないから厳密ではない
+        for key in outputs[0].keys():
+            # validation_stepのdictのkeyごとに集計
+            results = [outputs[i][key].cpu().numpy() for i in range(len(outputs))]
+            self.log(f"val_{key}", sum(results) / len(results))
 
     def test_step(self, batch, batch_idx):
         return self.validation_step(batch, batch_idx)
