40d7d45a310c4627f7ef9e8ad23ece3af215c5f9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 00:11:51 +0000

    Adding tarfile member sanitization to extractall()

--- 5-segmentation_voc/py/VOC2012Downloader.py
+++ 5-segmentation_voc/py/VOC2012Downloader.py
@@ -46,7 +46,26 @@ class VOC2012Downloader:
     def decompress(self):
         filepath = os.path.join(self.root_dir, self._COMPRESSED_FILE)
         with tarfile.open(filepath, "r:*") as tr:
-            tr.extractall(path=os.path.join(self.root_dir, ""))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tr, path=os.path.join(self.root_dir,""))
         os.remove(filepath)
 
     def make_raw_annotation(self):
